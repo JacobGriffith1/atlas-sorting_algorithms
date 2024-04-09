a64f1dca07c48e8f11ec38df6e81746d49d86aa2
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-09 04:51:02 +0000

    selection sort, 2-O

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -6,3 +6,25 @@
  * @array: Input array of integers
  * @size: Size of the array
  * Return: N/A
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	int temp;
+	size_t i, j, k;
+
+	for (i = 0; i < size; i++)
+	{
+		k = i;
+		for (j = i + 1; j < size; j++)
+			if (array[k] > array[j])
+				k = j;
+		if (i != k)
+		{
+			temp = array[i];
+			array[i] = array[k];
+			array[k] = temp;
+			print_array(array, size);
+		}
+	}
+}
