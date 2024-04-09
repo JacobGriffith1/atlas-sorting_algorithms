56954e4df5574b70f133049e6315f36b8886cc15
JacobGriffith1 <jacobgriffith6339@gmail.com>
2024-04-09 05:17:43 +0000

    quick sort, 3-O

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,3 +1,59 @@
 #include "sort.h"
 
 /**
+ * quick_sort - Sorts an array of integers in ascending order using the
+ * Quick sort algorithm.
+ * @array: Input array.
+ * @size: Size of the array.
+ * Return: N/A
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	_quick(array, 0, size - 1, size);
+}
+
+/**
+ * _quick - Helper function for the quick_sort function.
+ * @in: Input array.
+ * @lo: Index of first element.
+ * @hi: Index of last element.
+ * @size: Size of the array.
+ * Return: N/A
+ */
+
+void _quick(int *in, int lo, int hi, int size)
+{
+	int h, l, i;
+	int temp;
+
+	if (lo < hi)
+	{
+		h = hi;
+		l = lo;
+		for (i = lo; i < hi; i++)
+		{
+			if (in[i] < in[h])
+			{
+				if (i != l)
+				{
+					temp = in[i];
+					in[i] = in[l];
+					in[l] = temp;
+					print_array(in, size);
+				}
+				l++;
+			}
+		}
+		if (l != h && in[l] != in[h])
+		{
+			temp = in[l];
+			in[l] = in[h];
+			in[h] = temp;
+			print_array(in, size);
+		}
+		_quick(in, lo, l - 1, size);
+		_quick(in, l + 1, hi, size);
+	}
+}
+
--- sort.h
+++ sort.h
@@ -24,5 +24,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+void _quick(int *in, int lo, int hi, int size);
 
 #endif
